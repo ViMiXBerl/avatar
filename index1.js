0058936e583f057ee4bbac4b5f92c32172b40c25
Victoria <berling2818@gmail.com>
2021-02-11 21:39:26 +0300

    pagin

--- index1.js
+++ index1.js
@@ -1,5 +1,31 @@
 let page1 = document.querySelector("#page1");
 let page2 = document.querySelector("#page2");
+let urlFirstPage = "https://reqres.in/api/users";
+
+fetch(urlFirstPage)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let array = data.data;
+
+    let tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
+    array.forEach((item, index) => {
+      tbody.innerHTML += `<tr>
+       <td id = "${array[index].id}">
+       <img src="${item.avatar}" alt="avatar">
+       </td>
+       <td id = "${array[index].id}">${item.email}</td>
+       <td id = "${array[index].id}">${item.first_name}</td>
+       <td id = "${array[index].id}">${item.last_name}</td>
+  
+       </tr>`;
+    });
+  })
+  .catch(function (err) {
+    return err;
+  });
 
 page1.addEventListener("click", (event) => {
   event.preventDefault();
