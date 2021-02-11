ad81e7372ffec67b739ddd3af6f44ff8e89728ba
Victoria <berling2818@gmail.com>
2021-02-11 21:16:12 +0300

    pagin

--- index1.js
+++ index1.js
@@ -1,16 +1,51 @@
-let url = "https://reqres.in/api/users";
+let page1 = document.querySelector("#page1");
+let page2 = document.querySelector("#page2");
 
-fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    let array = data.data;
+page1.addEventListener("click", (event) => {
+  event.preventDefault();
+  let urlFirstPage = "https://reqres.in/api/users";
 
-    let tbody = document.querySelector("#tbody");
+  fetch(urlFirstPage)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let array = data.data;
 
-    array.forEach((item, index) => {
-      tbody.innerHTML += `<tr>
+      let tbody = document.querySelector("#tbody");
+      tbody.innerHTML = "";
+      array.forEach((item, index) => {
+        tbody.innerHTML += `<tr>
+       <td id = "${array[index].id}">
+       <img src="${item.avatar}" alt="avatar">
+       </td>
+       <td id = "${array[index].id}">${item.email}</td>
+       <td id = "${array[index].id}">${item.first_name}</td>
+       <td id = "${array[index].id}">${item.last_name}</td>
+  
+       </tr>`;
+      });
+    })
+    .catch(function (err) {
+      return err;
+    });
+});
+
+page2.addEventListener("click", (event) => {
+  event.preventDefault();
+  let urlSecondPage = "https://reqres.in/api/users?page=2&per_page=6";
+
+  fetch(urlSecondPage)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let array = data.data;
+
+      let tbody = document.querySelector("#tbody");
+      tbody.innerHTML = "";
+      array.forEach((item, index) => {
+        tbody.innerHTML += `<tr>
      <td id = "${array[index].id}">
      <img src="${item.avatar}" alt="avatar">
      </td>
@@ -19,8 +54,9 @@ fetch(url)
      <td id = "${array[index].id}">${item.last_name}</td>
 
      </tr>`;
+      });
+    })
+    .catch(function (err) {
+      return err;
     });
-  })
-  .catch(function (err) {
-    return err;
-  });
+});
